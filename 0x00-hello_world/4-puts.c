9ceeb64eb248d0ac4d5280f62961607c73b0f98b
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-03 05:58:42 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Description: Prints the description well
+*Return: often  zero(successful)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
