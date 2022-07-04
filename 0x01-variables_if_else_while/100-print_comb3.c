eb1b66bba5bde2889cc08b17aa4fa5cc1433f8e7
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-03 20:13:45 -0700

    we there to solve issues by doing hard things

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Gbeti B
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0 success.
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; num++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
