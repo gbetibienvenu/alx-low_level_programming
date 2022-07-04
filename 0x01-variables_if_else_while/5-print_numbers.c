1cb29bda402a3b42f77a0d05685ef3b50f88b11b
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-03 19:20:02 -0700

    let us have some good

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Bienvenu gbeti
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: zero always
+ */
+int main(void)
+{
+	int sum;
+
+	for (sum = 0; sum < 10; sum++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
