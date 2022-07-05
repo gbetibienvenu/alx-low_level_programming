b019138369340491c31ed028d2732f8757788749
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-05 12:07:52 -0700

    Wao this is pretty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,15 @@
 */
 int main(void)
 {
-	char bien[9] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i = i + 1)
-	{
-		_putchar(bien[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
