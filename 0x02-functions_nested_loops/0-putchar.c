9dcf5e97f5e79190ce666f29ec6b62f22af2d788
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-05 05:33:06 -0700

    This another way of doing programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*Main - Entry point
+*Description: Print _putchar and a newline
+*Return:  always 0
+*/
+int main(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is still a low level programmung but we are at the function nested level
