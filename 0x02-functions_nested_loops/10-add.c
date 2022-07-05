ce4fd9e8b1570ae07e0255174f1571316fa9ccfa
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-05 13:48:40 -0700

    things must be done in the right way

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Addition of two int
+ *
+ * a: first integer should added here
+ *
+ * b: second integer shoold also be added 
+ *
+ * Return: An pretty out put
+ */
+int add(int d, int c)
+{
+	return (d + c);
+}
