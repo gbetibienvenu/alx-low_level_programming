ad7f9a6f7dffaf7c35c3a135a6e8f01ffe12c2fd
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-05 05:45:33 -0700

    my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,15 @@
 */
 int main(void)
 {
-	putchar('_putchar');
-	putchar('\n');
-	return(0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
