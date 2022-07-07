f493d8c85dd5562c6d314b02e847353b92932a30
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-07 07:58:26 -0700

    Let us get the multiplication result as out put

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-mul.c
+ * Auth: Bienvenu G
+ */
+
+#include "main.h"
+
+/**
+ * mul - Multiples two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return: The multiplication results.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
