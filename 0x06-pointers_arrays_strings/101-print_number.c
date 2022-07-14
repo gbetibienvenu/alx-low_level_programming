fab0312add65c23e75a141349c519d66e092447b
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-14 10:02:08 -0700

    Write a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,33 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+/**
+ * print_number - print int
+ * @n: integer
+ * Return: Nothing
+ */
+void print_number(int n)
 {
-  int n;
-  int a[5];
-  int *p;
+	unsigned int v, d, count;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	if (n < 0)
+	{
+		_putchar(45);
+		v = n * -1;
+	}
+	else
+	{
+	v = n;
+	}
+	d = v;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((v / count) % 10) + 45);
+	}
 }
