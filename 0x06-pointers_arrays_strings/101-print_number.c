c9f4ee0c840cfa6807b61cc11df998f5e79c99e6
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-14 10:36:38 -0700

    We commiting

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,33 +1,26 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Bien
+ */
+
 #include "main.h"
 
 /**
- * print_number - print int
- * @n: integer
- * Return: void
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+		num = -num;
 	}
-	else
-	{
-		m = n;
-	}
-	d = m;
-	count = 1;
 
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 45);
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
