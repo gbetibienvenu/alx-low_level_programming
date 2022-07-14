d87fdfb0de233cb700605d4aabb6b32cd002048e
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-14 07:46:23 -0700

    This is a string task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,27 @@
-/*
- * File: 0-strcat.c
- * Auth: Bienvenu
- */
-
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
- *          null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
- *
- * Return: A pointer to the destination string @dest.
+ * *_strcat - concatenates two string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int count = 0, count1 = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	while (count1 >= 0)
+	{
+		*(dest + count) = *(src + count1);
+		if (*(src + count1) == '\0')
+			break;
+		count++;
+		count1++;
+	}
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,2 +1,1 @@
-This readme file concerning the low level programming stage for Pointer more Array .This is week 4 for our program at ALX
-
+This is our readme file for this particalur project
