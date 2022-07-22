3ea797d2c519ecb6c44e7837c5e597329479ca40
gbetibienvenu <bienvenugbeti7@gmail.com>
2022-07-22 14:12:45 -0700

    this is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: command line arg number
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
